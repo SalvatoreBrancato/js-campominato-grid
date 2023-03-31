377fc6919c340e6a2a7c36831afd3686cf015907
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-03-31 19:21:57 +0200

    Bonus_1_con_prompt

--- assets/main.js
+++ assets/main.js
@@ -6,19 +6,97 @@ function creaElementoHtml(tagHtml, classi, text){
     return elemento;
 }
 
-//variabile che richiama la classe div del HTML
+//variabile che richiama la classe div del HTML dove verranno inseriti i nuovi div
 let griglia = document.querySelector(".griglia");
 
+ const difficoltà1 = prompt("Inserisci il valore di difficoltà da 1 a 3 (1 hard, 2 middle, 3 easy)");
 //Ciclo che crea gli elementi HTML, richiamando la funzione precedente
-for(let i = 1; i <=100; i++){
-    const divbox = creaElementoHtml("div", "box", i);
-    griglia.append(divbox)
 
-    divbox.addEventListener("click", function(){
+ if(difficoltà1 == 1){
+     for(let i = 1; i <=100; i++){
+         const divbox = creaElementoHtml("div", "box", i);
+         griglia.append(divbox)
+    
+         divbox.addEventListener("click", function(){
+    
+             this.classList.toggle("blue")
+             this.classList.toggle("textwhite")
+             console.log(i)
+    
+         })
+     }
+ }else if(difficoltà1 == 2){
+     for(let i = 1; i <=81; i++){
+         const divbox = creaElementoHtml("div", "box2", i);
+         griglia.append(divbox)
+   
+         divbox.addEventListener("click", function(){
+    
+             this.classList.toggle("blue")
+             this.classList.toggle("textwhite")
+             console.log(i)
+    
+         })
+     }
+ }else if(difficoltà1 == 3){
+     for(let i = 1; i <=49; i++){
+         const divbox = creaElementoHtml("div", "box3", i);
+         griglia.append(divbox)
+   
+         divbox.addEventListener("click", function(){
+    
+             this.classList.toggle("blue")
+             this.classList.toggle("textwhite")
+             console.log(i)
+    
+         })
+     }
+ }
 
-        this.classList.toggle("blue")
-        this.classList.toggle("textwhite")
-        console.log(i)
+//Prova tramite select => non funzionante
 
-    })
-}
+
+// const play = document.querySelector("#gioca");
+// play.addEventListener("click", function(){
+//     let livello = document.querySelector("#difficoltà").value
+//     if(livello == 1){
+//         for(let i = 1; i <=100; i++){
+//             const divbox = creaElementoHtml("div", "box", i);
+//             griglia.append(divbox)
+                    
+//             divbox.addEventListener("click", function(){
+        
+//                 this.classList.toggle("blue")
+//                 this.classList.toggle("textwhite")
+//                 console.log(i)
+        
+//             })
+//         }
+//     }else if(livello == 2){
+//         for(let i = 1; i <=81; i++){
+//             const divbox = creaElementoHtml("div", "box2", i);
+//             griglia.append(divbox)
+        
+//             divbox.addEventListener("click", function(){
+        
+//                 this.classList.toggle("blue")
+//                 this.classList.toggle("textwhite")
+//                 console.log(i)
+        
+//             })
+//         }
+//     }else if(livello == 3){
+//         for(let i = 1; i <=49; i++){
+//             const divbox = creaElementoHtml("div", "box3", i);
+//             griglia.append(divbox)
+        
+//             divbox.addEventListener("click", function(){
+        
+//                 this.classList.toggle("blue")
+//                 this.classList.toggle("textwhite")
+//                 console.log(i)
+        
+//             })
+//         }
+//     }
+// })
